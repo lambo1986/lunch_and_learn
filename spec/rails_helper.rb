d6a0487c8feb5be63ed3f8259953c21131c67e86
Nathan Lambertson <lambertones@gmail.com>
2024-03-01 16:18:11 -0600

    Setup more gems and first test for a service

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,7 +77,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  # config.filter_sensitive_data('<YELP>') {Rails.application.credentials.yelp[:key]}
+  config.filter_sensitive_data('EDAMAM') { Rails.application.credentials[:api_key][:recipes] }
   # config.allow_http_connections_when_no_cassette = true
   # config.default_cassette_options = { record: :all }
 end
---
+++ spec/services/recipes_service_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe RecipeService, vcr: true, type: :service do
+  describe "methods" do
+    it "returns a list of recipes for an input country" do
+      
+    end
+  end
+end
