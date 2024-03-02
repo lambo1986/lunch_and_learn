37e09d197f59992f87769b47d30bd9383641a196
Nathan Lambertson <lambertones@gmail.com>
2024-03-02 15:19:39 -0600

    Test some SAD paths

--- spec/requests/api/v1/user_log_in_spec.rb
+++ spec/requests/api/v1/user_log_in_spec.rb
@@ -14,5 +14,31 @@ RSpec.describe "User Session", type: :request do
       expect(response.status).to eq(200)
       expect(json_response["data"]["attributes"]["api_key"]).not_to be_nil
     end
+
+    it "fails to create a new user session if password is not correct" do
+      user = User.create!(name: "George E. Boy", email: "coolby@thousands.com", password: "fghjkl", password_confirmation: "fghjkl")
+      post "/api/v1/sessions", params: {
+        email: user.email,
+        password: "pass123"
+      }.to_json, headers: { "Content-Type" => "application/json", "Accept" => "application/json" }
+
+      json_response = JSON.parse(response.body)
+
+      expect(response.status).to eq(401)
+      expect(json_response["error"]).to eq("Invalid email or password")
+    end
+
+    it "fails to create a new user session if email is not correct" do
+      user = User.create!(name: "George E. Boy", email: "coolby@thousands.com", password: "fghjkl", password_confirmation: "fghjkl")
+      post "/api/v1/sessions", params: {
+        email: "jerryjerry@dingleboy.com",
+        password: "fghjkl"
+      }.to_json, headers: { "Content-Type" => "application/json", "Accept" => "application/json" }
+
+      json_response = JSON.parse(response.body)
+
+      expect(response.status).to eq(401)
+      expect(json_response["error"]).to eq("Invalid email or password")
+    end
   end
 end
