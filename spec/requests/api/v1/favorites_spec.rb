d606f18409fb41ff7aa0f9380cf0d997b84ca858
Nathan Lambertson <lambertones@gmail.com>
2024-03-03 09:10:10 -0600

    Begin test for retrieving favs

--- spec/requests/api/v1/favorites_spec.rb
+++ spec/requests/api/v1/favorites_spec.rb
@@ -46,10 +46,19 @@ RSpec.describe "favorite recipes", type: :request do
       post "/api/v1/favorites", params: fav_params, headers: { "Content-Type" => "application/json", "Accept" => "application/json" }
 
       errors = JSON.parse(response.body)["errors"]
-      
+
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.status).to eq(422)
       expect(errors).to include("Country can't be blank", "Recipe link can't be blank", "Recipe title can't be blank")
+    end
+  end
+
+  describe "GET /api/v1/favorites" do
+    it "returns all favorites belonging to a user" do
+      user = User.create!(name: "Hansi P. Schmultzta", email: "smk@oolk.net", password: "craf!3G", password_confirmation: "craf!3G")
+      favorite1 = user.favorites.create!(country: "Germany", recipe_link: "https://sdfsdfsdf.com", recipe_title: "Hot Sausage and Sour Kraut")
+      favorite2 = user.favorites.create!(country: "Iceland", recipe_link: "https://sdfsdfsdf.vom/iceland", recipe_title: "Hot Soupy Stuff and Rotten Fish")
+      
     end
   end
 end
