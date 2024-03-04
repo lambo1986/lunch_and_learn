8b69d29ebcdd04f796ba25137df6ccbd7cc8ad98
Nathan Lambertson <lambertones@gmail.com>
2024-03-04 10:28:47 -0600

    Utilize my facade and services in controller

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::AirQualityController < ApplicationController
 
   def index
+    capital = CountryService.new.capital_of(params[:country])
+    coords = CoordsFacade.new.find_coords(capital, nil)
+    air_quality = PollutionFacade.new.pollution_at_coords(coords.first, coords.last)
     require 'pry'; binding.pry
-    capital = 
   end
 end
