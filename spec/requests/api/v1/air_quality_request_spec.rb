091f5370e76859ad5150bca13f34423ba81c4fef
Nathan Lambertson <lambertones@gmail.com>
2024-03-04 10:52:39 -0600

    Quintuple test the logic for country's air quality

--- spec/requests/api/v1/air_quality_request_spec.rb
+++ spec/requests/api/v1/air_quality_request_spec.rb
@@ -10,5 +10,41 @@ RSpec.describe "pollution requests", vcr: true, type: :request do
       expect(response).to have_http_status(:ok)
       expect(json_response["data"]["attributes"]["readable_aqi"]).to be_a(String)
     end
+
+    it "can do it again with another country" do
+      get "/api/v1/air_quality?country=Kenya"
+
+      json_response = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:ok)
+      expect(json_response["data"]["attributes"]["readable_aqi"]).to be_a(String)
+    end
+
+    it "can do it again with another Peru" do
+      get "/api/v1/air_quality?country=Peru"
+
+      json_response = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:ok)
+      expect(json_response["data"]["attributes"]["readable_aqi"]).to be_a(String)
+    end
+
+    it "can do it again with another France" do
+      get "/api/v1/air_quality?country=France"
+
+      json_response = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:ok)
+      expect(json_response["data"]["attributes"]["readable_aqi"]).to be_a(String)
+    end
+
+    it "can do it again with another Iceland" do
+      get "/api/v1/air_quality?country=Iceland"
+
+      json_response = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:ok)
+      expect(json_response["data"]["attributes"]["readable_aqi"]).to be_a(String)
+    end
   end
 end
